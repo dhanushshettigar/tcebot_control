2391076b4b6f22e7883b4efd174d2d1de8aaa7e3
dhanushshettigar <dhanushshettigar90@gmail.com>
2025-03-04 14:40:31 +0530

    bug fix

--- launch/tcebot_control.launch.py
+++ launch/tcebot_control.launch.py
@@ -1,56 +1,12 @@
 import os
 
-from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import Command
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
-    controller_params_file = os.path.join(get_package_share_directory("tcebot_control"),'config','tcebot_controllers.yaml')
-
-    # We need the robot description to be passed to the controller_manager
-    # So it can check the ros2_control parameters.
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[{'robot_description': ParameterValue(robot_description, value_type=str)},
-                    controller_params_file],
-        remappings=[
-            ('/diff_controller/cmd_vel', '/cmd_vel'), # Used if use_stamped_vel param is true
-            ('/diff_controller/cmd_vel_unstamped', '/cmd_vel'), # Used if use_stamped_vel param is false
-            ('/diff_controller/cmd_vel_out', '/cmd_vel_out'), # Used if publish_limited_velocity param is true
-            ('/diff_controller/odom', '/odom'),
-        ],
-        output="both",
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
-    )
-
-    diff_drive_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_controller", "--controller-manager", "/controller_manager"],
-    )
-
-    # Delay start of diff_drive_controller_spawner after `joint_state_broadcaster`
-    delay_diff_drive_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[diff_drive_controller_spawner],
-        )
-    )
-
+    # Declare arguments with default values
     port_arg = DeclareLaunchArgument(
         'port', default_value='/dev/ttyACM0',
         description='Serial port for communication with Arduino'
@@ -79,11 +35,7 @@ def generate_launch_description():
         }]
     )
 
-
     return LaunchDescription([
-        control_node,
-        joint_state_broadcaster_spawner,
-        delay_diff_drive_controller_spawner_after_joint_state_broadcaster_spawner,
         port_arg,
         wheel_diameter_arg,
         wheel_separation_arg,
